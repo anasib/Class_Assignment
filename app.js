4f885486337a16a1405161ba23ddf569aa5fe808
anasib <atta.nasib@bytecubed.com>
2018-05-05 11:13:12 -0400

    pseudo coded

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+var tablePath = "";
+var reservationPath = "";
+var waitingList = "";
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/api/tables', (req, res) =>
+    res.sendFile(tablePath, 'tables.html'));
+
+app.get('/api/reservations', (req, res) =>
+    res.sendFile(reservationPath, 'reservation.html'));
+
+app.get('/api/waiting', (req, res) =>
+    res.sendFile(waitingList, 'waitlist.html'));
+
+//fill in
+app.post('/api/tables', (req, res) => console.log(res));
+
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
-
